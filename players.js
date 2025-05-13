ba16c2c4b83cabae546aad9b492a42afb5ee3606
YasminaN1 <526ynigmanova@frhsd.com>
2025-05-13 09:30:24 -0400

    first commit

---
+++ players.js
@@ -0,0 +1,49 @@
+const players = [
+    {
+        firstName: 'Francisco',
+        lastName: 'Lindor',
+        position: 'SS',
+        age: '25',
+        photo: 'https://picsum.photos/200/300'
+    },
+
+    {
+        firstName: 'Francisco',
+        lastName: 'Lindor',
+        position: 'SS',
+        age: '25',
+        photo: 'https://picsum.photos/200/300'
+    },
+
+    {
+        firstName: 'Francisco',
+        lastName: 'Lindor',
+        position: 'SS',
+        age: '25',
+        photo: 'https://picsum.photos/200/300'
+    },
+
+    {
+        firstName: 'Francisco',
+        lastName: 'Lindor',
+        position: 'SS',
+        age: '25',
+        photo: 'https://picsum.photos/200/300'
+    },
+
+    {
+        firstName: 'Francisco',
+        lastName: 'Lindor',
+        position: 'SS',
+        age: '25',
+        photo: 'https://picsum.photos/200/300'
+    },
+
+    {
+        firstName: 'Francisco',
+        lastName: 'Lindor',
+        position: 'SS',
+        age: '25',
+        photo: 'https://picsum.photos/200/300'
+    },
+]
