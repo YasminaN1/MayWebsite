25c19801b87d908d7cda2d65c81d4a54313edebb
YasminaN1 <526ynigmanova@frhsd.com>
2025-05-29 10:07:46 -0400

    need to finish landin page

--- players.js
+++ players.js
@@ -1,49 +1,209 @@
 const players = [
     {
-        firstName: 'Francisco',
-        lastName: 'Lindor',
-        position: 'SS',
-        age: '25',
-        photo: 'https://picsum.photos/200/300'
+        firstName: 'Brett',
+        lastName: 'Berard',
+        position: 'Left Wing',
+        age: '23',
+        photo: 'imgs/BrettB.png'
     },
 
     {
-        firstName: 'Francisco',
-        lastName: 'Lindor',
-        position: 'SS',
-        age: '25',
-        photo: 'https://picsum.photos/200/300'
+        firstName: 'Jonny',
+        lastName: 'Brodzinski',
+        position: 'Center',
+        age: '32',
+        photo: 'imgs/JonnyB.png'
     },
 
     {
-        firstName: 'Francisco',
-        lastName: 'Lindor',
-        position: 'SS',
-        age: '25',
-        photo: 'https://picsum.photos/200/300'
+        firstName: 'Sam',
+        lastName: 'Carrick',
+        position: 'Center',
+        age: '32',
+        photo: 'imgs/SamC.png'
     },
 
     {
-        firstName: 'Francisco',
-        lastName: 'Lindor',
-        position: 'SS',
-        age: '25',
-        photo: 'https://picsum.photos/200/300'
+        firstName: 'Will',
+        lastName: 'Cuylle',
+        position: 'Left Wing',
+        age: '23',
+        photo: 'imgs/WillC.png'
     },
 
     {
-        firstName: 'Francisco',
-        lastName: 'Lindor',
-        position: 'SS',
+        firstName: 'Adam',
+        lastName: 'Edstrom',
+        position: 'Center',
         age: '25',
-        photo: 'https://picsum.photos/200/300'
+        photo: 'imgs/AdamE.png'
     },
 
     {
-        firstName: 'Francisco',
-        lastName: 'Lindor',
-        position: 'SS',
+        firstName: 'Arthur',
+        lastName: 'Kaliyev',
+        position: 'Forward',
+        age: '24',
+        photo: 'imgs/ArthurK.png'
+    },
+
+    {
+        firstName: 'Chris',
+        lastName: 'Kreider',
+        position: 'Left Wing',
+        age: '34',
+        photo: 'imgs/ChrisK.png'
+    },
+
+    {
+        firstName: 'Alexis',
+        lastName: 'Lafreniere',
+        position: 'Left Wing',
+        age: '24',
+        photo: 'imgs/AlexisL.png'
+    },
+
+    {
+        firstName: 'J.T.',
+        lastName: 'Miller',
+        position: 'Left Wing',
+        age: '32',
+        photo: 'imgs/J.T.M.png'
+    },
+
+    {
+        firstName: 'Brennan',
+        lastName: 'Othman',
+        position: 'Left Wing',
+        age: '22',
+        photo: 'imgs/BrennanO.png'
+    },
+
+    {
+        firstName: 'Artemi',
+        lastName: 'Panarin',
+        position: 'Left Wing',
+        age: '34',
+        photo: 'imgs/ArtemiP.png'
+    },
+
+    {
+        firstName: 'Juuso',
+        lastName: 'Parssinen',
+        position: 'Center',
+        age: '24',
+        photo: 'imgs/JuusoP.png'
+    },
+
+    {
+        firstName: 'Gabe',
+        lastName: 'Perreault',
+        position: 'Right Wing',
+        age: '20',
+        photo: 'imgs/GabeP.png'
+    },
+
+    {
+        firstName: 'Matt',
+        lastName: 'Rempe',
+        position: 'Center',
+        age: '23',
+        photo: 'imgs/MattR.png'
+    },
+
+    {
+        firstName: 'Vincent',
+        lastName: 'Trocheck',
+        position: 'Center',
+        age: '32',
+        photo: 'imgs/VincentT.png'
+    },
+
+    {
+        firstName: 'Mika',
+        lastName: 'Zibanejad',
+        position: 'Center',
+        age: '32',
+        photo: 'imgs/MikaZ.png'
+    },
+
+    {
+        firstName: 'Will',
+        lastName: 'Borgen',
+        position: 'Defenseman',
+        age: '28',
+        photo: 'imgs/WillB.png'
+    },
+
+    {
+        firstName: 'Calvin',
+        lastName: 'De Haan',
+        position: 'Defenseman',
+        age: '34',
+        photo: 'imgs/CalvinD.H..png'
+    },
+
+    {
+        firstName: 'Adam',
+        lastName: 'Fox',
+        position: 'Defenseman',
+        age: '27',
+        photo: 'imgs/AdamF.png'
+    },
+
+    {
+        firstName: 'Zac',
+        lastName: 'Jones',
+        position: 'Defenseman',
+        age: '24',
+        photo: 'imgs/ZacJ.png'
+    },
+
+    {
+        firstName: 'K Andre',
+        lastName: 'Miller',
+        position: 'Defenseman',
         age: '25',
-        photo: 'https://picsum.photos/200/300'
+        photo: 'imgs/KAndreM.png'
+    },
+
+    {
+        firstName: 'Braden',
+        lastName: 'Schneider',
+        position: 'Defenseman',
+        age: '23',
+        photo: 'imgs/BradenS.png'
+    },
+
+    {
+        firstName: 'Carson',
+        lastName: 'Soucy',
+        position: 'Defenseman',
+        age: '30',
+        photo: 'imgs/CarsonS.png'
+    },
+
+    {
+        firstName: 'Urho',
+        lastName: 'Vaakanienen',
+        position: 'Defenseman',
+        age: '26',
+        photo: 'imgs/UrhoV.png'
+    },
+
+    {
+        firstName: 'Jonathan',
+        lastName: 'Quick',
+        position: 'Goaltender',
+        age: '39',
+        photo: 'imgs/JonathanQ.png'
+    },
+
+    {
+        firstName: 'Igor',
+        lastName: 'Shesterkin',
+        position: 'Goaltender',
+        age: '29',
+        photo: 'imgs/IgorS.png'
     },
 ]
